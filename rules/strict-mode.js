b5fc77bd6ba1e6c0d3875bab3b191b83b64fd527
mikeskaife <mike@skaife.me>
2018-03-09 17:35:56 +0000

    Rule configuration for Strict Mode

---
+++ rules/strict-mode.js
@@ -0,0 +1,10 @@
+// https://eslint.org/docs/rules/#strict-mode
+
+module.exports = {
+
+    rules: {
+  
+      // require or disallow strict mode directives
+      strict: ['error', 'never']
+    }
+  };
