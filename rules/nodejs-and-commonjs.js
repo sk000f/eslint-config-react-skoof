63ca900a83898e643909327c15ebe91687fdb30a
mikeskaife <mike@skaife.me>
2018-03-09 23:24:37 +0000

    Rule configuration for NodeJS and CommonJS

---
+++ rules/nodejs-and-commonjs.js
@@ -0,0 +1,44 @@
+// https://eslint.org/docs/rules/#nodejs-and-commonjs
+
+/* eslint-disable no-undef */
+
+module.exports = {
+
+  rules: {
+
+    // require return statements after callbacks
+    // 'callback-return':
+
+    // require require() calls to be placed at top-level module scope
+    // 'global-require':
+
+    // require error handling in callbacks
+    // 'handle-callback-err':
+
+    // disallow use of the Buffer() constructor
+    // 'no-buffer-constructor':
+
+    // disallow require calls to be mixed with regular variable declarations
+    // 'no-mixed-requires':
+
+    // disallow new operators with calls to require
+    // 'no-new-require':
+
+    // disallow string concatenation with __dirname and __filename
+    // 'no-path-concat':
+
+    // disallow the use of process.env
+    // 'no-process-env':
+
+    // disallow the use of process.exit()
+    // 'no-process-exit':
+
+    // disallow specified modules when loaded by require
+    // 'no-restricted-modules':
+
+    // disallow synchronous methods
+    // 'no-sync':
+  }
+};
+
+/* eslint-enable no-undef */
