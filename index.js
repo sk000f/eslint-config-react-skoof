a5e363c28a6853b3ca01ac7e41228abc82a5b596
mikeskaife <mike@skaife.me>
2018-03-09 22:02:48 +0000

    Lintception!

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+
+/* eslint-disable no-undef */
+
 module.exports = {
-    extends: [
-      './rules/possible-errors',
-      './rules/best-practices',
-      './rules/strict-mode',
-      './rules/variables',
-      './rules/nodejs-and-commonjs',
-      './rules/stylistic-issues',
-      './rules/ECMAScript-6',
-      './rules/react',
-      './rules/react-a11y'
-    ],
-    rules: {}
+  extends: [
+    './rules/possible-errors',
+    './rules/best-practices',
+    './rules/strict-mode',
+    './rules/variables',
+    './rules/nodejs-and-commonjs',
+    './rules/stylistic-issues',
+    './rules/ECMAScript-6',
+    './rules/react',
+    './rules/react-a11y'
+  ].map(require.resolve),
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
+  rules: {},
 };
+
+/* eslint-enable no-undef */
